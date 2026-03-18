b52c1e3660517f0d0c60df1d543095783eff4963
Mihir Shetty <ms6556@columbia.edu>
2026-03-18 02:28:27 -0400

    Remove Shade

--- after/plugin/mason-lspconfig.lua
+++ after/plugin/mason-lspconfig.lua
@@ -4,6 +4,8 @@ require("lspconfig").clangd.setup({
   capabilities = capabilities,
 })
 
+vim.lsp.enable("julials")
+
 require("mason-lspconfig").setup {
     ensure_installed = { "clangd","rust_analyzer", "jedi_language_server", "gopls" },
 }
@@ -18,4 +20,28 @@ vim.api.nvim_create_autocmd("CursorHold", {
     }
     vim.diagnostic.open_float(nil, opts)
   end
+})
+
+-- Create an autocommand that runs when an LSP attaches to a buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    -- Replicating the new Neovim defaults
+    vim.keymap.set('n', 'grn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'gra', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'grr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gri', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'grt', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'gO', vim.lsp.buf.document_symbol, opts)
+    
+    -- Classic overrides/additions
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('i', '<C-S>', vim.lsp.buf.signature_help, opts)
+
+    -- Diagnostic navigation
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  end,
 })
--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,38 +1,40 @@
-vim.g.mapleader =" "
-vim.keymap.set("n", "<leader>ft", vim.cmd.Ex) -- open file tree
+    vim.g.mapleader =" "
+    vim.keymap.set("n", "<leader>ft", vim.cmd.Ex) -- open file tree
 
--- allows you to move highlighted sections as one
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+    -- allows you to move highlighted sections as one
+    vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
+    vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Keeps cursor in place when using J
-vim.keymap.set("n", "J", "mzJ'z")
+    -- Keeps cursor in place when using J
+    vim.keymap.set("n", "J", "mzJ'z")
 
--- Key cursor fixed when half paging
-vim.keymap.set("n","<C-d>", "<C-d>zz")
-vim.keymap.set("n","<C-u>", "<C-u>zz")
+    -- Key cursor fixed when half paging
+    vim.keymap.set("n","<C-d>", "<C-d>zz")
+    vim.keymap.set("n","<C-u>", "<C-u>zz")
 
--- keep cursor fixed when jumping searching
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+    -- keep cursor fixed when jumping searching
+    vim.keymap.set("n", "n", "nzzzv")
+    vim.keymap.set("n", "N", "Nzzzv")
 
--- when copy pasting, keeps copied value in buffer instead of overwritten value
-vim.keymap.set("x", "<leader>p" , "\"_dp")
+    -- when copy pasting, keeps copied value in buffer instead of overwritten value
+    vim.keymap.set("x", "<leader>p" , "\"_dp")
 
--- copys vim yank buffer to system buffer (ie. you can copy paste out of vim)
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+    -- copys vim yank buffer to system buffer (ie. you can copy paste out of vim)
+    vim.keymap.set("n", "<leader>y", "\"+y")
+    vim.keymap.set("v", "<leader>y", "\"+y")
+    vim.keymap.set("n", "<leader>Y", "\"+Y")
 
--- same, but with delete
-vim.keymap.set("n", "<leader>d", "\"+d")
-vim.keymap.set("v", "<leader>d", "\"+d")
+    -- same, but with delete
+    vim.keymap.set("n", "<leader>d", "\"+d")
+    vim.keymap.set("v", "<leader>d", "\"+d")
 
--- Q turns on some really mode of vim. Not useful nowadays, so neuter it
-vim.keymap.set("n","Q","<nop>")
+    -- Q turns on some really mode of vim. Not useful nowadays, so neuter it
+    vim.keymap.set("n","Q","<nop>")
 
--- Q turns on some really mode of vim. Not useful nowadays, so neuter it
-vim.keymap.set("n","s","d")
+    -- Q turns on some really mode of vim. Not useful nowadays, so neuter it
+    vim.keymap.set("n","s","d")
 
--- Allows you to find and replace the currently highlighted word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+    -- Allows you to find and replace the currently highlighted word
+    vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+
